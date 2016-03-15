e8b8273f7ef755fb4a95cd92599f3875389f8c2c
Phat Le <ltanphat@gmail.com>
2016-03-15 09:24:12 +0700

    Add Almofire lib

--- Podfile
+++ Podfile
@@ -4,4 +4,5 @@ use_frameworks!
 
 target :'ShortenImage' do
   pod 'FPPicker', '~> 5.1.3'
+  pod 'Alamofire', '~> 3.0'
 end
---
+++ ShortenImage/ShortenImageNetwork.swift
@@ -0,0 +1,9 @@
+//
+//  ShortenImageNetwork.swift
+//  ShortenImage
+//
+//  Created by Phat Le on 3/15/16.
+//  Copyright © 2016 FourFi. All rights reserved.
+//
+
+class ShortenImageNetwork
